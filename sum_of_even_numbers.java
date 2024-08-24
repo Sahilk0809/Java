59fd2fd76d0a6d082178a355f2b6338a12e9d0ef
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-08-24 18:41:53 +0530

    Create sum_of_even_numbers.java

---
+++ sum_of_even_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfEven {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value: ");
+        n = sc.nextInt();
+        int user = n;
+        sumOfEven(n, sum, user);
+    }
+    static void sumOfEven(int n, int sum, int user) {
+
+        if(n > 0){
+            sum = sum + n;
+            n--;
+            sumOfEven(n, sum, user);
+        } else {
+            System.out.println("Sum of even numbers between 1 to "+user +": " + sum);
+        }
+    }
+}
