6f551db74d54c3052ad57eaf1b65cbdcb20b5858
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-08-24 19:14:22 +0530

    Update prime_numbers.java

--- prime_numbers.java
+++ prime_numbers.java
@@ -9,7 +9,7 @@ public class Main {
         val = scanner.nextInt();
 
         for (int i = 1; i < val; i++) {
-            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0 || i == 2 || i == 3 || i == 5 || i == 7) {
                 System.out.println(i);
                 sum += i;
             }
