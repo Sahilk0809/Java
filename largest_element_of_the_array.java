9b2792b30adf0de12aace9b8ad53f87b3af7d2ab
Sahil Kashyap <149374235+Sahilk0809@users.noreply.github.com>
2024-08-24 18:49:13 +0530

    Create largest_element_of_the_array.java

---
+++ largest_element_of_the_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LargestArrayElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int max = arr[0];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The largest element is " + max);
+    }
+}
